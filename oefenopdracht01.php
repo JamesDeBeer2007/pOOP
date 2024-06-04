f30d52577ad047c06f50a4872659b9d3f5ba6560
James <9022370@student.zadkine.nl>
2024-06-04 11:36:05 +0200

    opdracht01

---
+++ oefenopdracht01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     class Product
+     {
+
+     }
+
+     $chair1 = new Product();
+     $chair2 = new Product();
+     $chair3 = new Product();
+
+     var_dumb($chair1);
+     var_dumb($chair2);
+     var_dumb($chair3);
+    ?>
+</body>
+</html>
