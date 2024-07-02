7761ae33b70a7ead3157d4c6bb0c8ac4f4c848ac
James <9022370@student.zadkine.nl>
2024-07-02 14:20:45 +0200

    opdracht 4

--- opdracht04.php
+++ opdracht04.php
@@ -3,6 +3,17 @@ class Product
 {
     public $name;
     public $price;
+    public $category;
+
+    public function setName($name)
+    {
+        $this->name = strtolower($name);
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = strtoupper($category);
+    }
 
     public function formatPrice()
     {
@@ -17,14 +28,17 @@ class Product
 }
 
 $game1 = new Product();
-$game1->name = "fifa 2024";
+$game1->setName("Fifa 2024");
 $game1->price = 40;
+$game1->setCategory("Sports");
 
 $game2 = new Product();
-$game2->name = "call of poop";
+$game2->setName("Call of Poop");
 $game2->price = 10;
+$game2->setCategory("Action");
 
 echo "Naam: " . $game2->name . "<br>";
+echo "Categorie: " . $game2->category . "<br>";
 echo "Prijs zonder BTW: " . $game2->formatPrice() . "<br>";
 echo "Prijs inclusief BTW: " . $game2->showSalesPrice() . "<br>";
 
